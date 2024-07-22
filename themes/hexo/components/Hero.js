4c42a165dcca62caecc011a781a07996a587f8e9
suyun <sylearn@foxmail.com>
2024-07-22 22:40:56 +0800

    Revert "内置AOS和TypedJS，避免CDN网络问题"
    This reverts commit d4c9f2c3fed156ad61a22aacc5e16b57aa2dcd1b.

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -26,7 +26,10 @@ const Hero = props => {
     updateHeaderHeight()
 
     if (!typed && window && document.getElementById('typed')) {
-      loadExternalResource('/js/typed.min.js', 'js').then(() => {
+      loadExternalResource(
+        'https://cdn.bootcdn.net/ajax/libs/typed.js/2.0.12/typed.min.js',
+        'js'
+      ).then(() => {
         if (window.Typed) {
           changeType(
             new window.Typed('#typed', {
