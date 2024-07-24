60f3ddfae5998c287ca02a83545fe7d6c8c6b2c4
suyun <sylearn@foxmail.com>
2024-07-24 15:49:55 +0800

    关闭遮罩 in Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -78,6 +78,7 @@ function Banner(props) {
       id='banners'
       onClick={handleClickBanner}
       className='hidden xl:flex xl:flex-col group h-full bg-white dark:bg-[#1e1e1e] rounded-xl border dark:border-gray-700 mb-3 relative overflow-hidden'>
+
       <div id='banner-title' className='flex flex-col absolute top-10 left-10'>
         <div className='text-4xl font-bold mb-3  dark:text-white'>
           {siteConfig('HEO_HERO_TITLE_1', null, CONFIG)}
@@ -92,20 +93,24 @@ function Banner(props) {
       {/* 斜向滚动的图标 该组件在这里被注释掉就不会有滚动图标 组件图标在public/images/heo*/}
        <TagsGroupBar /> 
 
-      {/* 遮罩 */}
+      {/* 遮罩 
       <div
         id='banner-cover'
         style={{ backdropFilter: 'blur(15px)' }}
         className={
           'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#4259efdd] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
         }>
+
         <div className='ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in'>
+
           <div className='text-7xl text-white font-extrabold'>随便逛逛</div>
+
           <div className='-ml-3 text-gray-300'>
             <ArrowSmallRight className={'w-24 h-24 stroke-2'} />
           </div>
         </div>
       </div>
+    */}
     </div>
   )
 }
@@ -120,7 +125,7 @@ function TagsGroupBar() {
   )
 
   return (
-    <div className='tags-group-all flex -rotate-[45deg] h-full'>
+    <div className='tags-group-all flex -rotate-[0deg] h-full'>
       <div className='tags-group-wrapper flex flex-nowrap absolute top-16'>
         {groupIcons?.map((g, index) => {
           return (
